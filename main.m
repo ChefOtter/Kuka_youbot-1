2eebb372877060ee1f5b349c5a8843efbb31a491
SrikanthMalla <malla.srikanth2012@vit.ac.in>
2017-04-18 02:47:05 -0400

    forward kinematics plot added in main

--- main.m
+++ main.m
@@ -1,17 +1,27 @@
 clc
 clear all
 close all
+%x=[q1,q2,q3,q4,dq1,dq2,dq3,dq4]
 x_0=rand(8,1);
 x_f=rand(8,1);
 %robustControl(x_0,x_f)
 tf = 2;
 
 % Generate Trajectories
-traj = [];
+coeff = [];
 j = 1;
 for i=1:4
-    traj = [traj;generateTrajectory(x_0(j:j+1),x_f(j:j+1), tf)];
+    coeff = [coeff;generateTrajectory(x_0(j:j+1),x_f(j:j+1), tf)'];
     j = j + 2;
 end
+theta_d=[];
+for t=0:0.01:tf
+    vec_t = [1; t; t^2; t^3]; % cubic polynomials
+    theta_d= [theta_d, [coeff(1,:)*vec_t; coeff(2,:)*vec_t; coeff(3,:)*vec_t; coeff(4,:)*vec_t;]];
+end
 
-passivity_normal(traj, tf);
+for j = 1:size(theta_d,2)
+    ee_pose = forwardKin([0;theta_d(:,j)]);
+    position(:,j) = ee_pose(1:3,4);
+end
+    % passivity_normal(traj, tf);
