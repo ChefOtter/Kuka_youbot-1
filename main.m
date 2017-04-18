49413eb70a309c64d502270eb9f12c36c6b28901
SrikanthMalla <malla.srikanth2012@vit.ac.in>
2017-04-17 22:13:34 -0400

    main file changed

--- main.m
+++ main.m
@@ -1,12 +1,6 @@
 clc
 clear all
 close all
-
-q = rand(5,1);
-qd = rand(5,1);
-
-M = getM(q);
-C = getC(q,qd);
-N = get_N(q);
-
-[A,B] = state_space(M,C,N,[q;qd]);
+x_0=rand(8,1);
+x_f=rand(8,1);
+robustControl(x_0,x_f)
